1484b85e122910de62c2c0792b62f4d305de48b4
Adi Kurniawan <daimagine@gmail.com>
2015-08-11 17:44:04 +0700

    update url production conf

--- src/app/config/production.js
+++ src/app/config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  AppRoot: "http://127.0.0.1:3334/",
-  APIRoot: "http://localhost:3000/api/v1",
+  AppRoot: "http://dev.app.aff.jualio.com",
+  APIRoot: "http://dev.api.aff.jualio.com/api/v1",
   ShortlrAPIRoot: "https://dev.app.jualio.com/apis"
 };
