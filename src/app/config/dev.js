980e452a5f7b191570d784852b4058ac53268c24
Adi Kurniawan <daimagine@gmail.com>
2015-08-19 23:45:48 +0700

    add social_media row

--- src/app/config/dev.js
+++ src/app/config/dev.js
@@ -2,4 +2,11 @@ module.exports = {
   AppRoot: "http://127.0.0.1:3334/",
   APIRoot: "http://localhost:3000/api/v1",
   ShortlrAPIRoot: "https://dev.app.jualio.com/apis/affiliate"
+
+  SocmedType: {
+    Twitter: 1,
+    Facebook: 2,
+    FacebookPage: 3,
+    Instagram: 4
+  }
 };
--- src/app/config/production.js
+++ src/app/config/production.js
@@ -2,4 +2,11 @@ module.exports = {
   AppRoot: "http://dev.app.aff.jualio.com",
   APIRoot: "http://dev.api.aff.jualio.com/api/v1",
   ShortlrAPIRoot: "https://dev.app.jualio.com/apis/affiliate"
+
+  SocmedType: {
+    Twitter: 5,
+    Facebook: 6,
+    FacebookPage: 7,
+    Instagram: 8
+  }
 };
